4777e630bcc6d1b96aa32635a43b5c51dff935a1
Truong Gia Linh <nhoquenroi2002@gmail.com>
2022-10-04 22:41:28 +0700

    xml file

---
+++ src/main/java/org/aibles/xml/XmlFileApplication.java
@@ -0,0 +1,13 @@
+package org.aibles.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aibles/xml/XmlFileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
